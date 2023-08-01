245e7e3f44b47fd2675518595aa414a098b23162
SooYoungJi <80882259+SooYoungJi@users.noreply.github.com>
2023-08-01 23:17:41 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,16 +1,8 @@
-'''def solution(citations):
-    answer = 0
-    citations.sort()
-    for i in range(len(citations)-1, 0, -1):
-        h = len(citations)-i
-        if citations[i] >= h:
-            answer = h
-    return answer'''
-
 def solution(citations):
     answer = 0
-    citations.sort(reverse=True)
-    for i in citations:
-        if i>answer:
-            answer+=1
-    return answer
+    citations.sort()
+    for i in range(len(citations)):
+        h = len(citations)-i
+        if citations[i] >= h:
+            return h
+    return 0
