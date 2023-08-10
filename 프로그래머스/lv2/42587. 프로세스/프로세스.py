98a9cc4c3a664457df6636f0e4846bfb72509f7e
SooYoungJi <80882259+SooYoungJi@users.noreply.github.com>
2023-08-10 16:06:02 +0900

    [level 2] Title: 프로세스, Time: 0.13 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.py
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -1,4 +1,17 @@
-from collections import deque
+def solution(priorities, location):
+    queue =  [(i,p) for i,p in enumerate(priorities)]
+    answer = 0
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
+
+
+'''from collections import deque
 def solution(priorities, location):
     answer = 0
     my_doc = [0 for _ in range(len(priorities))]
@@ -20,7 +33,7 @@ def solution(priorities, location):
 
 
     return answer
-    
+    '''
 '''  
     
     turn = []
