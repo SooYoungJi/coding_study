10b43980060b4ca34effa1dad6f7ab433a3d1405
SooYoungJi <80882259+SooYoungJi@users.noreply.github.com>
2023-09-08 21:18:37 +0900

    [unrated] Title: 숫자 변환하기, Time: 299.40 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,19 +1,3 @@
-MAX = 1_000_000
-INF = float('inf')
-
 def solution(x, y, n):
     answer = 0
-    
-    dp = [INF] * (MAX+1)
-    dp[x] = 0
-    
-    for i in range(x, y):
-        if dp[y] != INF:
-            return dp[y]
-        
-        for new_n in (i+n, i*2, i*3):
-            if MAX < new_n:
-                continue
-            dp[new_n] = min(dp[new_n], dp[i]+1)
-        
-    return dp[y] if dp[y] != INF else -1
+    return answer
