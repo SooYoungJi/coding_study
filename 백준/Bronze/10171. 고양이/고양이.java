285a75daad50f3cde93641da1c3175d0c5e4f175
SooYoungJi <80882259+SooYoungJi@users.noreply.github.com>
2024-02-07 23:51:53 +0900

    [Bronze V] Title: 고양이, Time: 72 ms, Memory: 11388 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\\    /\\").append("\n");
+		sb.append(" )  ( ')").append("\n");
+		sb.append("(  /  )").append("\n");
+		sb.append(" \\(__)|");
+		System.out.println(sb);
+	}
+
+}
