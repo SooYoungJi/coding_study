8d2309466fe76d4832057aa334aa19598b6b2b67
Sooyoung Ji <80882259+SooYoungJi@users.noreply.github.com>
2024-04-03 00:01:21 +0900

    [D4] Title: [Professional] 구간 합, Time: 123 ms, Memory: 26,180 KB -BaekjoonHub

--- SWEA/D4/5604. ［Professional］ 구간 합/［Professional］ 구간 합.java
+++ SWEA/D4/5604. ［Professional］ 구간 합/［Professional］ 구간 합.java
@@ -6,49 +6,54 @@ import java.util.StringTokenizer;
 
 public class Solution {
 	
-	static long number[], result,start, end, mul;
+	static long number[], result, start, end, mul;
 	static void cal(long a) {
-		for (long i = a; i > 0; i /= 10) {
+		for (long i = a; i > 0; i/=10) {
 			String s = Long.toString(i);
 			int t = s.charAt(s.length()-1)-'0';
-			number[t]+=mul;				
+			number[t]+=mul;
 		}
 	}
-
-	public static void main(String[] args) throws Exception {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int test = Integer.parseInt(br.readLine().trim());
-		for (int t = 1; t <= test; t++) {
-			String s = br.readLine();
-			StringTokenizer st = new StringTokenizer(s);
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			sb.append("#").append(tc).append(" ");
+			st = new StringTokenizer(br.readLine());
 			start = Long.parseLong(st.nextToken());
 			end = Long.parseLong(st.nextToken());
-			// 초기화
+			
 			number = new long[10];
 			result = 0;
 			mul = 1;
-			if(start==0) start=1;
-			while (start <= end) {
-				while(start%10!=0 && start<=end) {
+			if(start == 0) start = 1;
+			while(start<=end) {
+				while(start%10!=0 && start <= end) {
 					cal(start);
 					start++;
 				}
 				if(start>end) break;
-				while(end%10!=9 && start<=end) {
+				while(end%10!=9 && start <= end) {
 					cal(end);
 					end--;
 				}
-				long diff = end/10 - start/10 +1;
-				for(int i=0;i<10;i++)
-					number[i]+=diff*mul;
-				mul*=10;
+				long diff = end/10 - start/10 + 1;
+				for (int i = 0; i < 10; i++) {
+					number[i] += diff*mul;
+				}
+				mul *= 10;
 				start/=10;
 				end/=10;
-			}		
-			for (int i = 1; i < 10; i++)
-				result += (i * number[i]);
-			System.out.println("#" + t + " " + result);
+			}
+			for (int i = 0; i < 10; i++) {
+				result +=(i*number[i]);
+			}
+			sb.append(result).append("\n");
 		}
+		System.out.println(sb);
 	}
-
 }
