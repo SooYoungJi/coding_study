16db833988621243ac91058e5f41bf851fd5b59c
Sooyoung Ji <80882259+SooYoungJi@users.noreply.github.com>
2024-04-04 11:26:11 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,274 ms, Memory: 102,028 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -3,73 +3,47 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * @author taeheekim
+*/
 public class Solution {
-	
-	static int N, M, adj[][], cnt;
-	
+
+	static int N,M,adj[][];
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine().trim());
-		for (int tc = 1; tc <= T; tc++) {
-			N = Integer.parseInt(br.readLine().trim());
-			M = Integer.parseInt(br.readLine().trim());
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int TC = Integer.parseInt(in.readLine().trim());
+		for(int tc=1; tc<=TC; ++tc) {
+			N = Integer.parseInt(in.readLine());
+			M = Integer.parseInt(in.readLine());
+			adj = new int[N + 1][N + 1];// 0행, 0열 메모 목적으로 1씩 크기 늘림 (각행 0열 : 자신보다 큰 대상 개수, 0행 각열 : 자신보다 작은 대상  개수)
 			
-			adj = new int [N+1][N+1];
-			for (int i = 1; i <= N; i++) {
-				adj[i][0] = -1;
+			for (int m = 0; m < M; ++m) {
+				StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+				int i = Integer.parseInt(st.nextToken());
+				int j = Integer.parseInt(st.nextToken());
+				adj[i][j] = 1;
 			}
-			
-			StringTokenizer st = null;
-			for (int i = 0; i < M; i++) {
-				st = new StringTokenizer(br.readLine()," ");
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				adj[a][b] = 1;
-			}
-			
-			for (int i = 1; i <= N; i++) {
-				if(adj[i][0] == -1) dfs(i);
-			}
-			
-			for (int i = 1; i <= N; i++) {
-				for (int j = 1; j <= N; j++) {
-					adj[0][j] += adj[i][j];
-				}
-			}
-			
-			int ans = 0;
-			for (int i = 1; i <= N; i++) {
-				if(adj[i][0]+adj[0][i] == N-1) ++ans;
-			}
-			
-			System.out.println("#"+tc+" "+ans);
-		}
-	}
-	
-	private static void dfs(int cur) {
-		for (int i = 1; i <= N; i++) {
-			if(adj[cur][i] == 1) {
-				if(adj[i][0] == -1) { // 탐색이 안된 학생 탐색하기
-					dfs(i);
-				}
-				// i 학생보다 큰 학생이 있다면 그 관계들을 자신과의 관계에 반영
-				if(adj[i][0] > 0) {
-					for(int j=1; j <= N; j++) {
-						if(adj[i][j] == 1) { // i보다 큰 학생 j를 나 cur과의 관계로 표현
-							adj[cur][j] = 1;
-						}
+			int answer = 0;
+			for (int k=1; k<= N; ++k) {// 경유
+				for(int i=1; i<=N; ++i) { // 출발
+					if(i==k) continue;
+					for(int j=1; j<=N; ++j) { // 도착
+						if(adj[i][j]==1) continue;
+						adj[i][j] = adj[i][k] & adj[k][j] ; 
 					}
 				}
-				
 			}
+			for (int i = 1; i <= N; ++i) {
+				for (int j = 1; j <= N; ++j) {
+					adj[i][0] += adj[i][j]; // i보다 큰 j가 결국 카운트에 누적
+					adj[0][j] += adj[i][j]; // j보다 작은 i가 결국 카운트에 누적
+				}
+			}
+			for (int k = 1; k <= N; ++k) {
+				if (adj[k][0] + adj[0][k] == N - 1)
+					answer++;
+			}
+			System.out.println("#"+tc+" "+answer);
 		}
-		
-		int cnt = 0;
-		for (int i = 1; i <= N; i++) { // 자신보다 큰 학생 수 카운팅 (0은 덧셈에
-			cnt += adj[cur][i];
-		}
-		adj[cur][0] = cnt;
 	}
-	
-
 }
