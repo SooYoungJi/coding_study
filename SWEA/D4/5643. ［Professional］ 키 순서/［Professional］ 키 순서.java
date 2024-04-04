4ba9941197b11ab0ddd03e1ddb59e5a4501e6500
Sooyoung Ji <80882259+SooYoungJi@users.noreply.github.com>
2024-04-04 10:50:10 +0900

    [D4] Title: [Professional] 키 순서, Time: 495 ms, Memory: 103,064 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,102 +1,75 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
 	
-	static int N;
-	static int M;
-	
-	static List<Integer>[] boardUp;
-	static List<Integer>[] boardDown;
-	
-	static boolean[] visited;
+	static int N, M, adj[][], cnt;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine().trim());
 		for (int tc = 1; tc <= T; tc++) {
-			sb.append("#").append(tc).append(" ");
-			N = Integer.parseInt(br.readLine());
-			M = Integer.parseInt(br.readLine());
+			N = Integer.parseInt(br.readLine().trim());
+			M = Integer.parseInt(br.readLine().trim());
 			
-			boardUp = new ArrayList[N+1];
-			boardDown = new ArrayList[N+1];
-			
-			for (int i = 0; i <= N; i++) {
-				boardUp[i] = new ArrayList<Integer>();
-				boardDown[i] = new ArrayList<Integer>();
+			adj = new int [N+1][N+1];
+			for (int i = 1; i <= N; i++) {
+				adj[i][0] = -1;
 			}
 			
+			StringTokenizer st = null;
 			for (int i = 0; i < M; i++) {
-				st = new StringTokenizer(br.readLine());
+				st = new StringTokenizer(br.readLine()," ");
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
-				
-				boardUp[a].add(b);
-				boardDown[b].add(a);
+				adj[a][b] = 1;
 			}
-			
-			int answer = 0;
 			
 			for (int i = 1; i <= N; i++) {
-				int totalCnt = 0;
-				totalCnt += findUp(i);
-				totalCnt += findDown(i);
-				if(totalCnt == N-1) {
-					answer++;
+				if(adj[i][0] == -1) dfs(i);
+			}
+			
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= N; j++) {
+					adj[0][j] += adj[i][j];
 				}
 			}
 			
-			sb.append(answer).append("\n");
-		}
-		System.out.println(sb);
-	}
-	
-	// 위의 노드 탐색
-	public static int findUp(int node) {
-		int cnt = 0;
-		visited = new boolean[N+1];
-		Queue<Integer> queue = new ArrayDeque<>();
-		
-		queue.add(node);
-		while(!queue.isEmpty()) {
-			int curr = queue.poll();
-			for(int i : boardUp[curr]) {
-				if(!visited[i]) {
-					visited[i] = true;
-					cnt+=1;
-					queue.add(i);
-				}
+			int ans = 0;
+			for (int i = 1; i <= N; i++) {
+				if(adj[i][0]+adj[0][i] == N-1) ++ans;
 			}
+			
+			System.out.println("#"+tc+" "+ans);
 		}
-		return cnt;
 	}
 	
-	// 아래의 노드 탐색
-		public static int findDown(int node) {
-			int cnt = 0;
-			Queue<Integer> queue = new ArrayDeque<>();
-			
-			queue.add(node);
-			while(!queue.isEmpty()) {
-				int curr = queue.poll();
-				for(int i : boardDown[curr]) {
-					if(!visited[i]) {
-						visited[i] = true;
-						cnt+=1;
-						queue.add(i);
+	private static void dfs(int cur) {
+		for (int i = 1; i <= N; i++) {
+			if(adj[cur][i] == 1) {
+				if(adj[i][0] == -1) { // 탐색이 안된 학생 탐색하기
+					dfs(i);
+				}
+				// i 학생보다 큰 학생이 있다면 그 관계들을 자신과의 관계에 반영
+				if(adj[i][0] > 0) {
+					for(int j=1; j <= N; j++) {
+						if(adj[i][j] == 1) { // i보다 큰 학생 j를 나 cur과의 관계로 표현
+							adj[cur][j] = 1;
+						}
 					}
 				}
+				
 			}
-			return cnt;
 		}
+		
+		int cnt = 0;
+		for (int i = 1; i <= N; i++) { // 자신보다 큰 학생 수 카운팅 (0은 덧셈에
+			cnt += adj[cur][i];
+		}
+		adj[cur][0] = cnt;
+	}
+	
+
 }
