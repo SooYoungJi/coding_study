8c5b8c15b94156952ef3bcbbc8b88fe84ce055d6
Sooyoung Ji <80882259+SooYoungJi@users.noreply.github.com>
2024-05-05 15:03:33 +0900

    [D4] Title: 수영대회 결승전 ( 완전 탐색 + 구현 ), Time: 115 ms, Memory: 31,248 KB -BaekjoonHub

--- SWEA/D4/4193. 수영대회 결승전 （ 완전 탐색 ＋ 구현 ）/수영대회 결승전 （ 완전 탐색 ＋ 구현 ）.java
+++ SWEA/D4/4193. 수영대회 결승전 （ 완전 탐색 ＋ 구현 ）/수영대회 결승전 （ 완전 탐색 ＋ 구현 ）.java
@@ -1,45 +1,41 @@
+
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
 
+	static int minT;
 	static int N;
-	static int[][] swpool;
+	static int[][] board;
 	static boolean[][] visited;
+	static int[] start;
+	static int[] end;
+	// 상 하 좌 우 
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
 	
-	static int[] start, end;
-	
-	static int[] dr = {0, 0, 1, -1};
-	static int[] dc = {1, -1, 0, 0};
-	
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
-//		System.setIn(new FileInputStream("../aTestStudy/res/sample.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		StringBuilder sb = new StringBuilder();
 		
 		int T = Integer.parseInt(br.readLine());
-		
 		for (int tc = 1; tc <= T; tc++) {
 			sb.append("#").append(tc).append(" ");
+			
 			N = Integer.parseInt(br.readLine());
+			board = new int[N][N];
 			
-			swpool = new int[N][N];
-			
-			for (int i = 0; i < N; i++) {
+			for (int r = 0; r < N; r++) {
 				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < N; j++) {
-					swpool[i][j] = Integer.parseInt(st.nextToken());
+				for (int c = 0; c < N; c++) {
+					board[r][c] = Integer.parseInt(st.nextToken());
 				}
 			}
-			
-			
 			st = new StringTokenizer(br.readLine());
 			int r = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
@@ -49,53 +45,52 @@ public class Solution {
 			r = Integer.parseInt(st.nextToken());
 			c = Integer.parseInt(st.nextToken());
 			end = new int[] {r, c};
-			//--------------------Input End-------------------
 			
-			sb.append(bfs()).append("\n");
+			sb.append(swim()).append("\n");
 		}
 		System.out.println(sb);
-		
-		
 	}
-	private static int bfs() {
-		Queue<int[]> q = new LinkedList<int[]>();
+	
+	public static int swim() {
+		Queue<int[]> queue = new ArrayDeque<int[]>();
 		visited = new boolean[N][N];
-		q.add(start);
+		queue.offer(start);
 		visited[start[0]][start[1]] = true;
 		
-		int time = 0; // 탐색 시작 시간은 0초부터 시작
-		while(!q.isEmpty()) {
-			int size = q.size(); // 각 시간별로 탐색을 위한 사이즈 체크 
+		int time = 0;
+		
+		while(!queue.isEmpty()) {
+			int size = queue.size();
 			while(size-- > 0) {
-				int[] current = q.poll();
-				for (int d = 0; d < 4; d++) {
-					int nr = current[0]+dr[d];
-					int nc = current[1]+dc[d];
-					if(!inBound(nr, nc) || visited[nr][nc] || swpool[nr][nc] == 1) {
-						continue;
-					}
-					if(swpool[nr][nc] == 2) {
-						if(time%3!=2) {
-							q.offer(current.clone()); // 다음 위치 X
+				int[] curr = queue.poll();
+				for (int i = 0; i < 4; i++) {
+					int nr = curr[0] + dr[i];
+					int nc = curr[1] + dc[i];
+					if(!inBound(nr, nc)) continue;
+					if(visited[nr][nc]) continue;
+					if(board[nr][nc] == 1) continue;
+					if(board[nr][nc] == 2) {
+						if(time % 3 != 2) {
+							queue.offer(curr.clone());
 							continue;
 						}
 					}
-					
-					if(end[0] == nr && end[1] == nc) {
+					if(end[0] == nr && end[1]==nc) {
 						return time+1;
 					}
+					
 					visited[nr][nc] = true;
-					q.offer(new int[] {nr, nc});
+					queue.offer(new int[] {nr, nc});
 				}
 			}
 			time++;
 		}
-		return -1; 
+		return-1;
+		
 	}
 	
-	
 	public static boolean inBound(int nr, int nc) {
-		if(0 <= nr && nr < N && 0 <= nc && nc < N ) return true;
+		if(0 <= nr && nr < N && 0 <= nc && nc < N) return true;
 		else return false;
 	}
 
