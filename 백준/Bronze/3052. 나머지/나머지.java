8217a88c9e1cbda1576e3d2d15a9647c8a51746c
Sooyoung Ji <80882259+SooYoungJi@users.noreply.github.com>
2024-05-31 00:18:27 +0900

    [Bronze II] Title: 나머지, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		List<Integer> nums = new ArrayList<Integer>();
+		
+		for (int i = 0; i < 10; i++) {
+			int left = Integer.parseInt(br.readLine())%42;
+			if(!nums.contains(left)) {
+				nums.add(left);
+			}
+		}
+		System.out.println(nums.size());
+	}
+
+}
