8359b90a1df5b30d523125b85c8ab05c98f7fa84
Sooyoung Ji <80882259+SooYoungJi@users.noreply.github.com>
2024-05-30 23:01:47 +0900

    [Bronze II] Title: 숫자의 개수, Time: 76 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int total = 1;
+		for(int i = 0; i < 3; i++) {
+			total *= Integer.parseInt(br.readLine());	
+		}
+		int[] nums = new int[10];
+		while(total > 0) {
+			nums[total%10]++;
+			total /= 10;
+		}
+		for(int i : nums) {
+			System.out.println(i);
+		}
+	}
+
+}
