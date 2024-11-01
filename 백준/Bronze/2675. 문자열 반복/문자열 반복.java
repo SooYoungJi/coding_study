d0861e3025c82ef325a049276a19d496b86d1421
Sooyoung Ji <80882259+SooYoungJi@users.noreply.github.com>
2024-11-01 16:47:08 +0900

    [Bronze II] Title: 문자열 반복, Time: 68 ms, Memory: 11796 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,27 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int tc = 0; tc < T; tc++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int R = Integer.parseInt(st.nextToken());
-			char[] chars = st.nextToken().toCharArray();
-			for(char s: chars) {
-				for (int i = 0; i < R; i++) {
-					sb.append(Character.toString(s));
-				}
-			}
-			sb.append("\n");
-		}
-		System.out.println(sb);
-	}
-
+        int T = Integer.parseInt(br.readLine());
+        for(int tc = 0; tc < T; tc++){
+            String answer = "";
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            char[] chars = st.nextToken().toCharArray();
+            for(char c : chars){
+                for(int i = 0; i < n; i++){
+                    answer += Character.toString(c);
+                }
+            }
+            sb.append(answer).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
